8dfccb355deee072ebb9d418e4f89225a4e38e1d
Chris Sole-Kavanagh <chrissolekavanagh@gmail.com>
2024-06-13 13:53:00 +0100

    small ui changes

--- app/src/main/java/com/connectedid/whisperubmexample/MainActivity.kt
+++ app/src/main/java/com/connectedid/whisperubmexample/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.connectedid.whisperubmexample
 
 import android.Manifest
+import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
+import android.os.Vibrator
+import android.os.VibratorManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -25,6 +30,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -36,8 +42,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import com.connectedid.whisperubmexample.ui.theme.WhisperUBMExampleTheme
 import com.whisper.whisperubmandroid.WhisperUBM
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -50,18 +60,35 @@ import kotlinx.coroutines.launch
 class MainActivity : ComponentActivity() {
 
     var whisperUBM: WhisperUBM? = null
-    private val _uiState = MutableStateFlow(listOf<String>())
+    private val _uiState = MutableStateFlow(listOf<String>("test1","test2","test3","test4","test1","test2","test3","test4","test1","test2","test3","test4","test1","test2","test3","test4","test1","test2","test3","test4","test1","test2","test3","test4","test1","test2","test3","test4","test1","test2","test3","test4","test1","test2","test3","test4","test1","test2","test3","test4"))
     val uiState: StateFlow<List<String>> = _uiState.asStateFlow()
     val context = this
     private val _permission = MutableStateFlow(false)
     val permissionState: StateFlow<Boolean> = _permission.asStateFlow()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val vib = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val vibratorManager =
+                getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+            vibratorManager.defaultVibrator
+        } else {
+            @Suppress("DEPRECATION")
+            getSystemService(VIBRATOR_SERVICE) as Vibrator
+        }
+
+
+
         super.onCreate(savedInstanceState)
         requestMicPermission()
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
 
+            window?.statusBarColor = Color.Black.toArgb()
+
+            val configuration = LocalConfiguration.current
+            val screenHeight = configuration.screenHeightDp.dp
             var listening: Boolean? by remember{
                 mutableStateOf(null)
             }
@@ -70,7 +97,6 @@ class MainActivity : ComponentActivity() {
             WhisperUBMExampleTheme {
                 
                 Column (Modifier.background(Color.Black)){
-                    Spacer(modifier = Modifier.height(40.dp))
                     Box (
                         Modifier
                             .fillMaxWidth()
@@ -96,12 +122,12 @@ class MainActivity : ComponentActivity() {
                                             _uiState.update {
                                                 it + arrayListOf(ubmString)
                                             }
-                                            println(_uiState.value.toString())
+                                            vib.vibrate(200)
                                         }
                                         listening = false
                                     }catch (e: Exception){
                                         println(e)
-                                        //bad prefix/hash
+                                        vib.vibrate(200)
                                     }
                                 },
                                 permission = permission.value
@@ -109,8 +135,9 @@ class MainActivity : ComponentActivity() {
                             else -> {
                                 Box (
                                     Modifier
+                                        .align(Alignment.TopStart)
                                         .fillMaxWidth()
-                                        .fillMaxHeight(0.8f)
+                                        .height(screenHeight - 75.dp)
                                 ){
                                     MessageList(messages = uiState)
                                 }
@@ -172,23 +199,26 @@ fun MessageList(messages: StateFlow<List<String>>){
     ) {
         for (message in messagesState.value){
             ListRow(message = message)
-            SideEffect {
-                coroutineScope.launch {
-                    scrollState.scrollTo(messagesState.value.size -1)
-                }
-            }
         }
     }
+
+    LaunchedEffect(messagesState.value.size){
+        coroutineScope.launch {
+            scrollState.scrollBy(999999f) //why is there no .scrollToBottom()!?!?!?
+        }
+    }
+
 }
 
 @Composable
 fun OnOff(listening: Boolean?, startButton: () -> Unit, modifier: Modifier){
 
     Row(
-        modifier.padding(20.dp)
+        modifier//.padding(vertical = 40.dp, horizontal = 20.dp)
     ) {
         Box(
             modifier = Modifier
+                .fillMaxWidth()
                 .clip(RoundedCornerShape(25))
                 .size(65.dp)
                 .background(
@@ -220,12 +250,12 @@ fun OnOff(listening: Boolean?, startButton: () -> Unit, modifier: Modifier){
 @Composable
 fun SettingsRow(startButton: (String, String) -> Unit, modifier: Modifier, permission: Boolean){
     val sharedPref: SharedPreferences = LocalContext.current.getSharedPreferences("myPref", MODE_PRIVATE)
-    var hash = remember{ mutableStateOf(sharedPref.getString("hash", "") ?: "") }
-    var prefix = remember{ mutableStateOf(sharedPref.getString("prefix", "") ?: "") }
+    var hash = remember{ mutableStateOf(sharedPref.getString("hash", "8b0caca4276eb4bb99af09e2851b4063bf00510ee95d96235854d02a2f0f0073") ?: "") }
+    var prefix = remember{ mutableStateOf(sharedPref.getString("prefix", "B_00008_") ?: "") }
 
 
     Row(
-        modifier.padding(20.dp)
+        modifier
     ) {
 
         Column(
